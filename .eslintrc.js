4ec3ee534352f51f36b7fc0089289c99a0e1d651
m.garmabi <m.garmabi@khodro45.com>
2025-07-04 22:06:06 +0330

    refactor(package): update package.json to use ES modules and remove webpack configuration

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    '@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    // Add any specific rules here
+  },
+}; 
